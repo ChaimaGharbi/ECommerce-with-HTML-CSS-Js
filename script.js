fbfcd961968d94cdb78c23c0d10081dd82ef5742
ChaimaGharbi <Chaima.GH6602@gmail.com>
2023-03-27 21:19:20 +0100

    Shop and single product pages done

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ if (close) {
     nav.classList.remove("active");
   })
 }
+
+const path = window.location.pathname.split('/').pop();
+const navbar = document.getElementById("navbar");
+
+const prevActive = navbar.querySelector(".active");
+if (prevActive) {
+  prevActive.classList.remove("active");
+}
+
+if (path === "shop.html" || path === "sproduct.html") {
+  const shopLink = navbar.querySelector("a[href='shop.html']");
+  shopLink.classList.add("active");
+} else {
+  const current = navbar.querySelector(`a[href='${path}']`);
+  if (current) {
+    current.classList.add("active");
+  }
+}
+
+const products = document.getElementById('product1');
+    products.addEventListener('click', () => {
+      window.location.href = 'sproduct.html';
+})
